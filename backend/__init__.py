412f2093345a033c54a202145e3c2de396bcfb79
Ishpreet Kaur <ishpreetkaurkamboj7@gmail.com>
2026-01-10 22:59:45 +0530

    Refactor create_app to register blueprints in app context
    Updated create_app function to register blueprints within the application context for better organization.

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,19 +4,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL") 
+    
+    db.init_app(app) 
+    CORS(app)
 
-    # Initialize extensions
-    from .routes import routes_bp
-    app.register_blueprint(routes_bp)
-    # Register blueprints
-    from .auth import auth_bp
-    app.register_blueprint(auth_bp)
-   
-
+    with app.app_context():
+        from routes import routes_bp
+        from auth import auth_bp
+        app.register_blueprint(routes_bp)
+        app.register_blueprint(auth_bp)
 
     return app
-
-
